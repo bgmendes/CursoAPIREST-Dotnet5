2a53c636bbaa871355b890a14f26c16bae04605a
Bruno Mendes <bruunomendes@hotmail.com>
2023-04-20 20:06:34 -0300

    Connecting To Database

---
+++ 03_RestAPI-Dotnet5_ConnectingToDatabase/RestAPI-Dotnet5/Services/Implementations/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestAPI_Dotnet5.Model;
+using System.Collections.Generic;
+
+namespace RestAPI_Dotnet5.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
